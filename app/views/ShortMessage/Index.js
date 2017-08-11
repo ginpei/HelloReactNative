b0f0391f670f9c14a3205d623add12dba440a4d5
ginpei <ginpei@gmail.com>
2017-08-11 14:43:08 -0700

    short message for iOS
    Not checked. Probably it works finely.

--- app/views/ShortMessage/Index.js
+++ app/views/ShortMessage/Index.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import { ToastAndroid } from 'react-native';
+import { Platform, AlertIOS, ToastAndroid } from 'react-native';
 
 export default class ShortMessage {
 	static show(message, options = {}) {
 		console.log('# ShortMessage::show', message, options);
-		const duration = options.duration || ToastAndroid.SHORT;
-		ToastAndroid.show(message, duration);
+
+		if (Platform.os === 'ios') {
+			AlertIOS.alert('', message);
+		}
+		else {
+			const duration = options.duration || ToastAndroid.SHORT;
+			ToastAndroid.show(message, duration);
+		}
 	}
 }
