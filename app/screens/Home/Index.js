4b5e40225958638d8e8f54ff5925ed964831d77a
ginpei <ginpei@gmail.com>
2017-08-11 21:45:37 -0700

    manage data in from screen main

--- app/screens/Home/Index.js
+++ app/screens/Home/Index.js
@@ -7,11 +7,37 @@ import {
 } from 'react-native';
 import NoteTable from './NoteTable.js'
 import { DrawerNavigator } from 'react-navigation';
+import LoadingIndicator from '../../views/LoadingIndicator/Index.js';
+import Note from '../../models/Note.js'
+import ShortMessage from '../../views/ShortMessage/Index.js';
+import firebase from '../../config/firebase.js';
 
 export default class Home extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			loading: true,
+			notes: [],
+		};
+	}
+
+	componentWillMount() {
+		const user = firebase.user;
+		Note.fetchAllFor(user)
+			.then(notes => this.setState({ loading: false, notes: notes }))
+			.catch(error => {
+				ShortMessage.show(`Failed to fetch notes: ${error && error.message}`);
+			});
+	}
+
 	render() {
+		if (this.state.loading) {
+			return <LoadingIndicator />;
+		}
+
 		return (
 			<NoteTable
+				notes={this.state.notes}
 				onItemPress={(props) => this.onItemPress(props)} />
 		);
 	}
--- app/screens/Home/NoteTable.js
+++ app/screens/Home/NoteTable.js
@@ -6,42 +6,18 @@ import ReactNative, {
 	TouchableHighlight,
 } from 'react-native';
 
-import firebase from '../../config/firebase.js';
 import Note from '../../models/Note.js';
-import LoadingIndicator from '../../views/LoadingIndicator/Index.js';
 import CenteredMessage from '../../views/CenteredMessage/Index.js';
-import ShortMessage from '../../views/ShortMessage/Index.js';
 
 export default class Home extends React.Component {
-	constructor() {
-		super();
-
-		this.state = {
-			loading: true,
-			notes: [],
-		};
-	}
-
-	componentWillMount() {
-		const user = firebase.user;
-		Note.fetchAllFor(user)
-			.then(notes => this.setState({ loading: false, notes: notes }))
-			.catch(error => {
-				ShortMessage.show(`Failed to fetch notes: ${error && error.message}`);
-			});
-	}
-
 	render() {
-		if (this.state.loading) {
-			return <LoadingIndicator />;
-		}
-		else if (this.state.notes.length < 1) {
+		if (this.props.notes.length < 1) {
 			return <CenteredMessage message="No notes. 🍣" />;
 		}
 
 		return (
 			<FlatList
-				data={this.state.notes}
+				data={this.props.notes}
 				style={styles.list}
 				renderItem={(props) => this.renderListItem(props)} />
 		);
